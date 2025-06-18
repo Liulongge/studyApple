f15a765f1173a1a66b2bc622518bbbc8dc304375
RungeLiu <792286891@qq.com>
2025-06-19 01:31:00 +0800

    NEW: add Swift

---
+++ 00-SwiftUI/00-SwiftUI/00-SwiftUI/_0_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  _0_SwiftUIApp.swift
+//  00-SwiftUI
+//
+//  Created by runge.liu on 2025/6/19.
+//
+
+import SwiftUI
+
+@main
+struct _0_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ 00-SwiftUI/README.md
@@ -0,0 +1,2 @@
+
+https://www.bilibili.com/video/BV1TV4y1K7Ew?spm_id_from=333.788.videopod.sections&vd_source=6b48595092f05a0fc1d129f83872951f
